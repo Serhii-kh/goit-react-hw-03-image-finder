caf14b40925eb647bb839426169eab78f38293ab
Serhii Naboka <drifterzzz@gmail.com>
2023-05-04 21:01:01 +0300

    upd component Gallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,72 +4,78 @@ import { fetchImages } from 'Api/fetchImages';
 import css from './ImageGallery.module.css';
 
 export class ImageGallery extends Component {
-  state = {
-    searchQuery: '',
-    images: null,
-    error: null,
-    page: '',
-    status: 'idle',
-  };
+	state = {
+		images: null,
+		page: null,
+		error: null,
+		// status: 'idle',
+	};
 
-  async componentDidUpdate(prevProps, prevState) {
-    const prevSearchQuery = prevProps.searchQuery;
-    const nextSearchQuery = this.props.searchQuery;
+	componentDidUpdate(prevProps) {
+		const prevSearchQuery = prevProps.searchQuery;
+		const nextSearchQuery = this.props.searchQuery;
 
-    if (prevSearchQuery !== nextSearchQuery) {
-      this.setState({
-        images: [],
-        page: 1,
-      });
+		if (prevSearchQuery !== nextSearchQuery) {
+			this.setState({
+				// images: [],
+				page: 1,
+			});
 
-      try {
-        await fetchImages(nextSearchQuery, this.state.page).then(images =>
-          this.setState(prevState => ({
-            images,
-            page: prevState.page + 1,
-          }))
-        );
-      } catch (error) {
-        this.setState({ error });
-      }
-    }
-  }
+			try {
+				fetchImages(nextSearchQuery, this.state.page).then(images =>
+					this.setState(prevState => ({
+						images,
+						page: (prevState.page + 1),
+					}))
+				);
+			} catch (error) {
+				this.setState({ error });
+			}
+		}
+	}
 
-  handleLoadMoreBtnClick = async () => {
-    try {
-      await fetchImages(this.state.searchQuery, this.state.page).then(images =>
-        this.setState(prevState => ({
-          images,
-          page: prevState.page + 1,
-        }))
-      );
-    } catch (error) {
-      this.setState({ error });
-    }
-  };
+	handleLoadMoreBtnClick = () => {
+		try {
+			fetchImages(this.props.searchQuery, this.state.page).then(images =>
+				this.setState(prevState => ({
+					images: [...prevState.images, ...images],
+					page: (prevState.page + 1),
+				}))
+			);
+		} catch (error) {
+			this.setState({ error });
+		}
+	};
 
-  render() {
-    const { images } = this.state;
 
-    return (
-      <div>
-        {images && (
-          <>
-            <ul className={css.ImageGallery}>
-              {images.map(({ tags, webformatURL, id }) => (
-                <ImageGalleryItem key={id} src={webformatURL} alt={tags} />
-              ))}
-            </ul>
-            <button
-              type="button"
-              className={css.Button}
-              onClick={this.handleLoadMoreBtnClick}
-            >
-              Load more
-            </button>
-          </>
-        )}
-      </div>
-    );
-  }
+	// clearSate = () => {
+	// 	this.setState({
+	// 		page: 1,
+	// 	});
+	// };
+
+	render() {
+		const { images } = this.state;
+
+		return (
+			<div>
+				{images && (
+					<>
+						<ul className={css.ImageGallery}>
+							{images.map(({ tags, webformatURL, id }) => (
+								<ImageGalleryItem key={id} src={webformatURL} alt={tags} />
+							))}
+						</ul>
+						<button
+							type="button"
+							className={css.Button}
+							onClick={this.handleLoadMoreBtnClick}
+						>
+							Load more
+						</button>
+					</>
+				)}
+			</div>
+		);
+	}
 }
